91dacdfa43a9a490c42441c8bf9193cea583ab8f
Hacker-Code-J <hacker3740@gmail.com>
2024-04-29 20:51:45 +0900

    Laptop240429v1

--- haskell/monads-practice.py
+++ haskell/monads-practice.py
@@ -1,73 +1,94 @@
 # Reference:
 # https://nikgrozev.com/2013/12/10/monads-in-15-minutes/
 
-#=======================
-# Logging
-#=======================
+import timeit
+import matplotlib.pyplot as plt
 
-# def f1(x):
-#     return (x + 1, str(x) + "+1")
-# def f2(x):
-#     return (x + 2, str(x) + "+2")
-# def f3(x):
-#     return (x + 3, str(x) + "+3")
+def f1(x):
+    return (x + 1, str(x) + "+1")
 
-# log = "Ops:"
+def f2(x):
+    return (x + 2, str(x) + "+2")
 
-# x = 0
-# res, log1 = f1(x)
-# log += log1 + ";"
+def f3(x):
+    return (x + 3, str(x) + "+3")
 
-# res, log2 = f2(res)
-# log += log2 + ";"
+def unit(x):
+    return (x, "Ops:")
 
-# res, log3 = f3(res)
-# log += log3 + ";"
+def bind(t, f):
+    res = f(t[0])
+    return (res[0], t[1] + res[1] + ";")
 
-# print(res, log)
+# Direct calls setup
+def direct_calls():
+    x = 0
+    log = "Ops:"
+    res, log1 = f1(x)
+    log += log1 + ";"
+    res, log2 = f2(res)
+    log += log2 + ";"
+    res, log3 = f3(res)
+    log += log3 + ";"
+    return res, log
 
-# #--
+# Functional approach setup
+def functional_approach():
+    x = 0
+    return bind(bind(bind(unit(x), f1), f2), f3)
 
-# def unit(x):
-#     return (x, "Ops:")
+# Number of iterations
+num_runs = [100, 1000, 10000, 100000, 1000000, 10000000]
 
-# def bind(t, f):
-#     res = f(t[0])
-#     return (res[0], t[1] + res[1] + ";")
+# Timing data storage
+direct_times = []
+functional_times = []
 
-# print( bind(bind(bind(unit(x), f1), f2), f3) )
+'''
+This code will measure the execution time of each approach
+by running each 'n' times and return the total time taken for all runs.
+You can adjust the number of iterations
+based on how precise you want the measurements to be or
+based on your system's performance characteristics.
+'''
+# Measurement loop
+for n in num_runs:
+    direct_time = timeit.timeit(direct_calls, number=n) / n
+    functional_time = timeit.timeit(functional_approach, number=n) / n
+    direct_times.append(direct_time)
+    functional_times.append(functional_time)
 
-#=======================
-# List of Interim Values
-#=======================
+# Calculating speed differences
+# speed_differences = [direct - functional for direct, functional in zip(direct_times, functional_times)]
 
-# def f1(x): return x + 1
-# def f2(x): return x + 2
-# def f3(x): return x + 3
+# Plotting execution times
+plt.figure(figsize=(10, 5))
+plt.plot(num_runs, direct_times, label='Direct Calls', marker='o')
+plt.plot(num_runs, functional_times, label='Functional Approach', marker='x')
 
-# x = 0
-# lst = [x]
+# Adding text annotations for Direct Calls
+for i, txt in enumerate(direct_times):
+    plt.annotate(f'{txt:.1e}', (num_runs[i], direct_times[i]), textcoords="offset points", xytext=(0,10), ha='center')
 
-# res = f1(x)
-# lst.append(res)
+# Adding text annotations for Functional Approach
+for i, txt in enumerate(functional_times):
+    plt.annotate(f'{txt:.1e}', (num_runs[i], functional_times[i]), textcoords="offset points", xytext=(0,-15), ha='center')
 
-# res = f2(res)
-# lst.append(res)
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Number of Runs')
+plt.ylabel('Execution Time (seconds)')
+plt.title('Execution Times Comparison')
+plt.legend()
+plt.grid(True)
+plt.show()
 
-# res = f3(res)
-# lst.append(res)
-
-# print(res, lst)
-
-# def unit(x):
-#     return (x, [x])
-
-# def bind(t, f):
-#     res = f(t[0])
-#     return (res, t[1] + [res])
-
-# print( bind(bind(bind(unit(x), f1), f2), f3) )
-
-#=======================
-# Nulls/Nones
-#=======================
+# # Plotting speed difference
+# plt.figure(figsize=(10, 5))
+# plt.bar(range(len(num_runs)), speed_differences, tick_label=num_runs)
+# plt.xlabel('Number of Runs')
+# plt.ylabel('Speed Difference (seconds)')
+# plt.title('Speed Difference Between Direct Calls and Functional Approach')
+# plt.xticks(rotation=45)
+# plt.grid(True)
+# plt.show()
